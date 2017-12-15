46d9c80773150ec8613c6044f3468b577a33d38c
Nicole Thoen <nicolemthoen@gmail.com>
2017-12-14 22:02:39 -0500

    upload everything I have up to this point

---
+++ exercise-1/test.js
@@ -0,0 +1,11 @@
+require ('exercise1-original.js');
+require ('exercise.js');
+
+var assert = require('assert');
+describe('exercise1-original', function() {
+	//https://mochajs.org/
+});
+describe('exercise1-solution', function() {
+	return remoteMathService().should.eventually.equal(3);
+	//https://www.npmjs.com/package/chai-as-promised
+});
